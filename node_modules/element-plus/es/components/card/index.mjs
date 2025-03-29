7f2223ca33c3d6f8f0452ccd839b20e6e25680a8
zoutenghao <1539040972@qq.com>
2025-03-29 20:37:23 +0800

    Remove node_modules from git tracking

--- node_modules/element-plus/es/components/card/index.mjs
+++
@@ -1,8 +0,0 @@
-import Card from './src/card.mjs';
-export { cardProps } from './src/card2.mjs';
-import { withInstall } from '../../utils/vue/install.mjs';
-
-const ElCard = withInstall(Card);
-
-export { ElCard, ElCard as default };
-//# sourceMappingURL=index.mjs.map
--- node_modules/element-plus/es/components/result/index.mjs
+++
@@ -1,8 +0,0 @@
-import Result from './src/result.mjs';
-export { IconComponentMap, IconMap, resultProps } from './src/result2.mjs';
-import { withInstall } from '../../utils/vue/install.mjs';
-
-const ElResult = withInstall(Result);
-
-export { ElResult, ElResult as default };
-//# sourceMappingURL=index.mjs.map
